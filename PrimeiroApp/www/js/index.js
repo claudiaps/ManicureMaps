1122ce3e46a66ff3a510d954c4ee32448dc6a2d5
claudiaps <clp.sampedro@gmail.com>
2017-09-18 01:40:48 -0300

    novas mudanças

---
+++ PrimeiroApp/www/js/index.js
@@ -0,0 +1,23 @@
+function exibirCadastro(){
+	MobileUI.hide('formulario-login');
+	MobileUI.show('formulario-cadastro');
+}
+
+function exibirLogin(){
+	MobileUI.show('formulario-login');
+	MobileUI.hide('formulario-cadastro');
+}
+
+function logar(){
+	var usuario = MobileUI.objectByForm('formulario-login');
+	if (usuario.email == "" || usuario.senha == ""){
+		alert("Digite o seu e-mail e senha");
+	}
+	else{
+		openPage('lista');
+	}
+}
+
+function cadastrar(){
+	alert("teste")
+}
